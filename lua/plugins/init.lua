38d7051096ae0db19442cfcc5b86e127ba4e3410
chengzhycn <chengzhycn@gmail.com>
2025-03-30 18:13:49 +0800

    fix gitsigns error

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,7 +35,7 @@ end
 local fn = vim.fn
 
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-local remote_url = 'https://ghproxy.com/https://github.com/wbthomason/packer.nvim'
+local remote_url = 'https://github.com/wbthomason/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system('git clone --depth 1 '..remote_url.." "..install_path)
 end
